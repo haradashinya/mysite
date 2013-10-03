e3583093ede75ad1e713c10ed3ebdd38108ead3c
okamurayasuyuki <haradashinya@gmail.com>
2013-10-03 20:38:24 +0900

    fix

--- header.php
+++ header.php
@@ -5,6 +5,13 @@
         <title><?php wp_title('|',true,'right'); bloginfo('name');?></title>
         <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.9.1/build/cssreset/cssreset-min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+        <link rel="stylesheet" type="text/css" href="http://157.7.153.172/wp-content/themes/mysite/bootstrap/dist/css/bootstrap.css">
+
+        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+        <script src="http://157.7.153.172/wp-content/themes/mysite/bootstrap/dist/js/bootstrap.js"></script>
+
+
+    <?php echo get_stylesheet_uri(); ?>
 
     </head>
     <body>
