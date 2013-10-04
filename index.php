88810ae3275e94ead85a9277df57dec3cee6e979
okamurayasuyuki <haradashinya@gmail.com>
2013-10-04 15:40:46 +0900

    add foundation and use scss instead of css

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ if (have_posts()) :
     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
     </h2>
     <div class="post-meta">
-    <?php echo get_the_date(); ?> 【<?php the_category(', '); ?>】
     </div>
     </div>
     <div class="post-content">
