18b82329d25c7045bec2b2aa1d7b01eb3d159b84
okamurayasuyuki <haradashinya@gmail.com>
2013-10-23 19:31:49 +0900

    remove comm

--- functions.php
+++ functions.php
@@ -44,6 +44,14 @@ add_filter('excerpt_more', 'new_excerpt_more');
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_generator');
+
+/* インラインスタイル削除 */
+function remove_recent_comments_style() {
+    global $wp_widget_factory;
+    remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+}
+add_action( 'widgets_init', 'remove_recent_comments_style' );
 
 
 
