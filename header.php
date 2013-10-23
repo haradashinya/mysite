6c124f3ddb9abce29b3c3aafaed7b159add9701a
okamurayasuyuki <haradashinya@gmail.com>
2013-10-23 19:19:53 +0900

    fix

--- header.php
+++ header.php
@@ -3,17 +3,15 @@
     <head>
         <meta charset="utf-8">
         <title><?php wp_title('|',true,'right'); bloginfo('name');?></title>
-        <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.9.1/build/cssreset/cssreset-min.css">
-        <link rel="stylesheet" type="text/css" href="wp-content/themes/mysite/css/foundation.css">
+
         <link rel="stylesheet" type="text/css" href="wp-content/themes/mysite/css/reset.css">
+        <link rel="stylesheet" type="text/css" href="wp-content/themes/mysite/css/foundation.css">
+
         <link rel="stylesheet" type="text/css" href="wp-content/themes/mysite/css/style.css">
-        <link href='http://fonts.googleapis.com/css?family=Cantora+One' rel='stylesheet' type='text/css'>
 	<meta name="robots" content="nofollow">
 	<meta name="robots" content="noindex">
 
-
-
-        <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     </head>
 
     <body>
